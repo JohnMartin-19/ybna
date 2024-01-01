f92eb662bc9912fe76ca667d542c9be6a0a7fc65
John <johnnkonge2020@gmail.com>
2024-01-01 22:28:14 +0300

    fixed imports

--- Frontend/client/src/app/app.modules.ts
+++ Frontend/client/src/app/app.modules.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { AppRoutingModule} from './app-routing.module'
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+@NgModule({
+  declarations: [AppComponent]
+})
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration(), provideNoopAnimations()]
 };
